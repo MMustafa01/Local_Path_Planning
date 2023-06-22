94846b52d72b3e31b3700faba1450e1b3cd55d3b
Syed Mustafa <sm06654@st.habib.edu.pk>
2023-06-22 15:14:52 -0500

    exploration

--- CARLA_Codes/CARLA_exploration1.py
+++ CARLA_Codes/CARLA_exploration1.py
@@ -17,16 +17,70 @@ try:
 except IndexError:
     pass
 import carla
-
+10
 
 
 try:
     client = carla.Client('localhost', 2000)
     client.set_timeout(20.0)
 
+    '''
+    To change map use the following command
+    Note: 
+    1. It takes time to change the map, thus increase the timeout from 20 to 200
+    '''
+    # print(client.get_available_maps()) #This is to to check what maps are available
+    # world = client.load_world('/Game/Carla/Maps/Town10HD')
+
+    ###### Getting the world map #######
     world = client.get_world()
+    ###### blueprint is needed to get any actor #####
+    bp_lib = world.get_blueprint_library()
+    
+    spawn_points = world.get_map().get_spawn_points()
+    #### Try to get the coordinates through spawn points ####
+    print(f"The spawn points are given at: \t{spawn_points[0].location}")
+    '''Note to self:
+    1. __dir__() is a useful python method to get attributes
+    2. spawn points have an attribute .localtion that gives location information
+    '''
 
+    
+    ########################### Loading Modeles ################################
+    
+    vehicle = bp_lib.find('vehicle.lincoln.mkz_2020')
+    vehicle = world.try_spawn_actor(vehicle, random.choice(spawn_points)) # use the .try_spawn_actor method always since sometimes the actor may collide at that spawn point
+    
 
+    ############################ Specter Mode ##################################
+
+    t0 = time.clock()
+    while True:
+        spectator = world.get_spectator() 
+        transform = carla.Transform(vehicle.get_transform().transform(carla.Location(x=-4,z=2.5)),vehicle.get_transform().rotation) 
+        spectator.set_transform(transform) 
+
+        vehicle.set_autopilot(True)
+        tn = time.clock()
+        t_diff = tn - t0
+        print(t_diff)
+        if t_diff >= 60:
+            break
 
 finally:
+
+    try:
+        vehicle.destroy()
+    except:
+        print("could not delete vehice")
     print("The simulation is done")
+
+
+
+    '''
+    Things that will be used later
+    print(world.get_actors().find(471))
+    world.get_actors().find(471).destroy()
+    
+    
+    '''
